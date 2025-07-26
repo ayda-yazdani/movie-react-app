2fcd6d3f9508c0493eda4b79ac1881bd3068de48
Ayda Yazdani <aydayazdani@gmail.com>
2025-07-26 18:28:02 +0100

    Updated root layout with auth protection

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,26 +1,43 @@
+import { AuthProvider, useAuth } from "@/services/AuthContext";
 import { Stack } from "expo-router";
-import { StatusBar } from "react-native";
+import { StatusBar, Text, View } from "react-native";
 import './globals.css';
+
+function RootLayoutNav() {
+  const { user, isLoading} = useAuth()
+
+  // Show loading screen while checking auth
+  if (isLoading) {
+    return (
+      <View className="flex-1 justify-center items-center bg-primary">
+        <Text className="text-white">Loading...</Text>
+      </View>
+    )
+  }
+
+  return (
+    <>
+      <StatusBar hidden={true} />
+      <Stack screenOptions={{ headerShown: false}}>
+        {user ? (
+          // Authenticated user sees main app
+          <>
+            <Stack.Screen name="(tabs)" />
+            <Stack.Screen name="movies/[id]" />
+          </>
+        ): (
+          // Unauthenticated user sees auth screens
+          <Stack.Screen name="(auth)" />
+        )}
+      </Stack>
+    </>
+  )
+}
 
 export default function RootLayout() {
   return (
-  <>
-    <StatusBar hidden={true} />
-  <Stack>
-    
-    <Stack.Screen
-      name="(tabs)"
-      options={{ 
-        headerShown: false,
-      }}
-    />
-    <Stack.Screen
-      name="movies/[id]"
-      options={{ 
-        headerShown: false,
-      }}
-    />
-  </Stack>
-  </>
+    <AuthProvider>
+      <RootLayoutNav />
+    </AuthProvider>
   )
 }
